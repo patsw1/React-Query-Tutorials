dc6c4e54dfe5b98a760dde65b26d4d0b496860cc
Patrick Sweeney <psweeney@nyc.rr.com>
2024-05-12 20:51:03 -0400

    corrected

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,22 +1,20 @@
-import { useQuery  } from 'react-query'
-import axios from 'axios'
+import { useQuery } from "react-query";
+import axios from "axios";
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery('super-heros', () => {
-    return axios.get('http://localhost:4001/superheroes')
-  })
+  const { isLoading, data } = useQuery("super-heros", () => {
+    return axios.get("http://localhost:4001/superheroes");
+  });
 
   if (isLoading) {
-    return <h2>loading...</h2>
+    return <h2>loading...</h2>;
   }
   return (
-  <>
-  <h2>React Query Super Heroes Page</h2>
-  {
-    data?.data.data(hero => {
-    return <div key={hero.name}>{hero.name}</div>
-    })
-  }
-  </>
-  )
-}
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
