70cff4624188eb39be1f29f8e875884d427c91d3
patrick.sweeney@jpmchase.com <patsw2@gmail.com>
2024-05-12 20:20:35 -0400

    addtional

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,21 @@
+import { useQuery  } from 'react-query'
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
+  const { isLoading, data } = useQuery('super-heros', () => {
+    return axios.get('http://localhost:4001/superheroes')
+  })
+
+  if (isLoading) {
+    return <h2>loading...</h2>
+  }
+  return (
+  <>
+  <h2>React Query Super Heroes Page</h2>
+  {
+    data?.data.data(hero => {
+    return <div key={hero.name}>{hero.name}</div>
+    })
+  }
+  </>
+  )
 }
